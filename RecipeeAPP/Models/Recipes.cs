23fdf2065f396483c43ab0b8d33f42417aeeb3c7
A00233642 <84159018+A00233642@users.noreply.github.com>
2021-12-01 22:09:09 -0500

    Created some data model attribute for recipe

--- RecipeeAPP/Models/Recipes.cs
+++ RecipeeAPP/Models/Recipes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace RecipeeAPP.Models
 {
     public class Recipes
     {
+        [Key]
+        public int RecipeID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+
     }
 }
