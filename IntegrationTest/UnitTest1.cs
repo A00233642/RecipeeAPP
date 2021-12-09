2759a6f53240c35adf1cf655e117e32956157ad9
A00233642 <84159018+A00233642@users.noreply.github.com>
2021-12-09 18:56:47 -0500

    Started integration Testing

--- IntegrationTest/UnitTest1.cs
+++ IntegrationTest/UnitTest1.cs
@@ -1,13 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace IntegrationTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+       
+
+        EdgeDriver _driver;
+        private string _url = "https://localhost:44311/";
+
+        [TestInitialize]
+        public void Recipe()
         {
+            new DriverManager().SetUpDriver(new EdgeConfig());
+            var options = new EdgeOptions
+            {
+                PageLoadStrategy = PageLoadStrategy.Normal,
+                AcceptInsecureCertificates = true
+            };
+
+            _driver = new EdgeDriver(options);
+
         }
+
+       
     }
 }
